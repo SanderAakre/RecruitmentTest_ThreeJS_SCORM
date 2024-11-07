cd4e1b1afd70e2a295de64e3fedc125af82400a2
SanderAakre <sanderjoacimakre@gmail.com>
2024-11-07 15:50:14 +0100

    Init next.js

---
+++ vehicle-part-info/src/app/page.tsx
@@ -0,0 +1,12 @@
+'use client'
+
+import React from "react";
+import ThreeScene from "@/components/ThreeScene";
+
+export default function Home() {
+  return (
+    <div className="h-screen w-screen">
+      <ThreeScene />
+    </div>
+  );
+}
